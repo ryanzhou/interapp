e2863ec3f34db9d32c88e4ea7fa86ee3e3dc7079
Ryan Zhou <git@zhoutong.com>
2014-09-11 10:56:38 +1000

    work around Rails autoloading

--- lib/interapp/engine.rb
+++ lib/interapp/engine.rb
@@ -1,5 +1,8 @@
 module Interapp
   class Engine < ::Rails::Engine
     isolate_namespace Interapp
+    config.after_initialize do
+      require 'interapp/application_controller'
+    end
   end
 end
